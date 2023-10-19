767196ec273e4aad30f51266bf7e856c5fcb4d79
Ethan Berkebile <ethanberkebile@gmail.com>
2023-10-19 17:42:22 -0500

    imported band.js library. Needs refactoring

--- migration.sql
+++ migration.sql
@@ -4,13 +4,14 @@ DROP TABLE IF EXISTS service_branches;
 
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    name TEXT,
-    song_id INT,
-    veteran BOOLEAN,
+    first_name TEXT NOT NULL,
+    last_name, TEXT NOT NULL,
+    song_id INT NOT NULL,
+    veteran BOOLEAN NOT NULL,
     branch_id INT
 );
 
--- INSERT INTO users (num) VALUES (1);
+INSERT INTO users (first_name, last_name, song_id, veteran, branch_id) VALUES ('John', 'Smith', 1, true, 1);
 -- INSERT INTO users (num) VALUES (2);
 -- INSERT INTO users (num) VALUES (3);
 -- INSERT INTO users (num) VALUES (4);
@@ -21,8 +22,17 @@ CREATE TABLE songs (
     name TEXT
 );
 
+INSERT INTO songs (name) VALUES ('Batman'), ('Final Countdown'), ('Hawaii Five-O'), ('Jump'), ('Mario'), ('Superman');
+
 CREATE TABLE service_branches (
     id SERIAL PRIMARY KEY,
     name TEXT,
     imgurl TEXT
 );
+
+INSERT INTO service_branches (name, imgurl) VALUES ('Army', 'https://www.defense.gov/portals/1/Page-Assets/branding-guide/armed-forces/sealArmy.png');
+INSERT INTO service_branches (name, imgurl) VALUES ('Marine Corps', 'https://www.defense.gov/portals/1/Page-Assets/branding-guide/armed-forces/sealMarineCorps.png');
+INSERT INTO service_branches (name, imgurl) VALUES ('Navy', 'https://www.defense.gov/portals/1/Page-Assets/branding-guide/armed-forces/sealNavy.png');
+INSERT INTO service_branches (name, imgurl) VALUES ('Air Force', 'https://www.defense.gov/portals/1/Page-Assets/branding-guide/armed-forces/sealAirForce.png');
+INSERT INTO service_branches (name, imgurl) VALUES ('Space Force', 'https://www.defense.gov/portals/1/Page-Assets/branding-guide/armed-forces/SealSpaceForce.png');
+INSERT INTO service_branches (name, imgurl) VALUES ('Coast Guard', 'https://www.defense.gov/portals/1/Page-Assets/branding-guide/armed-forces/sealCoastGuard.png');
