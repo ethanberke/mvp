5860de808189fe520c7bd0ec1345c1dc38f47148
Ethan Berkebile <ethanberkebile@gmail.com>
2023-10-22 23:27:34 -0500

    Songs and Images append.

--- public/app.js
+++ public/app.js
@@ -3,21 +3,15 @@ import playSuperman from "./songs/superman.js"
 import playJump from "./songs/jump.js"
 import playBatman from "./songs/batman.js"
 import playHawaii from "./songs/hawaii5o.js"
+import playFinalCountDown from "./songs/finalcountdown.js"
 
-let playButton = document.querySelector(".playButton");
-playButton.addEventListener('click', function() {
-    // playMario();
-    // playSuperman();
-     playJump();
-    // playBatman();
-    // playHawaii();
-})
+
 
 const finduserNameInput = document.getElementById("findfname");
 const createUsernameInput = document.getElementById("createfname");
 const findBtn = document.getElementById("findBtn");
 const createBtn = document.getElementById("createBtn");
-const profileContainer = document.querySelector(".profileLookup");
+const profileContainer = document.querySelector(".profileContainer");
 const createProfileForm = document.querySelector(".createProfile-form");
 
 createProfileForm.addEventListener("submit", (event) => {
@@ -29,11 +23,11 @@ createProfileForm.addEventListener("submit", (event) => {
     const veteranInput = formData.get('ans') === 'yes' ? true : false;
     const branchIdMap = {
         'Army': 1,
-        'Marine Corps': 2,
+        'MarineCorps': 2,
         'Navy': 3,
-        'Air Force': 4,
-        'Space Force': 5,
-        'Coast Guard': 6
+        'AirForce': 4,
+        'SpaceForce': 5,
+        'CoastGuard': 6
     };
     const songIdMap = {
         'Batman': 1,
@@ -73,6 +67,26 @@ createProfileForm.addEventListener("submit", (event) => {
         console.error('Error creating profile:', error);
     });
 });
+//function to generate image based on profile branch.id
+function addImage(branchId) {
+    const image = document.createElement('img');
+
+    if (branchId === 1) {
+        image.src = 'https://www.defense.gov/portals/1/Page-Assets/branding-guide/armed-forces/sealArmy.png';
+    } else if (branchId === 2) {
+        image.src = 'https://www.defense.gov/portals/1/Page-Assets/branding-guide/armed-forces/sealMarineCorps.png';
+    } else if (branchId === 3) {
+        image.src = 'https://www.defense.gov/portals/1/Page-Assets/branding-guide/armed-forces/sealNavy.png';
+    } else if (branchId === 4) {
+        image.src = 'https://www.defense.gov/portals/1/Page-Assets/branding-guide/armed-forces/sealAirForce.png';
+    } else if (branchId === 5) {
+        image.src = 'https://www.defense.gov/portals/1/Page-Assets/branding-guide/armed-forces/SealSpaceForce.png';
+    } else if (branchId === 6) {
+        image.src = 'https://www.defense.gov/portals/1/Page-Assets/branding-guide/armed-forces/sealCoastGuard.png';
+    }
+    profileContainer.appendChild(image);
+}
+
 
 function searchProfiles(username) {
     fetch(`/profiles/${username}`)
@@ -81,6 +95,11 @@ function searchProfiles(username) {
         if (profile) {
             // Display the first profile from the response (assuming you want just one)
             console.log("Profile data:", profile);
+            const fullName = document.createElement('p');
+            fullName.textContent = (`${profile.first_name} ${profile.last_name}`);
+            profileContainer.appendChild(fullName);
+            addImage(profile.branch_id);
+            createPlayButton(profile.song_id);
         } else {
             console.log("No matching profiles found.");
         }
@@ -94,40 +113,32 @@ findBtn.addEventListener("click", function () {
     event.preventDefault();
     const findUsername = findusername.value;
     searchProfiles(findUsername);
+    createProfileForm.innerHTML = '';
+    profileContainer.innerHTML = '';
 });
 
-// function createProfileElement(profile) {
-//     const p = document.createElement("p");
-//     p.addEventListener("click", () => {
-//         fetch(`/profiles/${profile.id}`, {
-//             method: "DELETE",
-//         }).then(() => {
-//             p.remove();
-//         });
-//     });
-//     p.innerText = profile.submit;
-//     return p;
-// }
+function addSong(songId) {
+    if (songId === 1) {
+        playBatman();
+    } else if (songId === 2) {
+        playFinalCountDown();
+    } else if (songId === 3) {
+        playHawaii();
+    } else if (songId === 4) {
+        playJump();
+    } else if (songId === 5) {
+        playMario();
+    } else if (songId === 6) {
+        playSuperman();
+    }
+}
 
+function createPlayButton(songId) {
+    const playButton = document.createElement('button');
+    playButton.textContent = 'Play Song';
+    profileContainer.appendChild(playButton);
 
-
-// Function to retrieve profiles
-// function getProfile() {
-//     fetch("/profiles")
-//         .then((response) => response.json())
-//         .then((profiles) => {
-//             if (profiles.length === 0) {
-//                 console.log("No profiles found.");
-//             } else {
-//                 profiles.forEach((profile) => {
-//                     console.log("All profiles", profile)
-//                 });
-//             }
-//         })
-//         .catch((error) => {
-//             console.error('Error fetching profiles:', error);
-//         });
-// }
-
-// // Load profiles on page load
-// getProfile();
+    playButton.addEventListener('click', function() {
+        addSong(songId);
+    });
+}
